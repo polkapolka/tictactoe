bc2c8c76d707a1aec2b8feb9ec9cbcb25b28d0a2
Phebe Polk <phebepolk@gmail.com>
2018-09-12 18:22:15 -0700

    second try

--- tictactoe.py
+++ tictactoe.py
@@ -8,37 +8,52 @@ import sys
 def create_player(symbol):
     """Takes user input on player type and returns a Player."""
     while True:
-    	try:
-		    player_type = input("Is player %s human or AI?\nEnter \'H\' for human, \'A\' for AI: " % symbol)
-		except:
-			sys.exit(0)
-	    if player_type == 'H':
-	    	return HumanPlayer(symbol)
-	    elif player_type == 'A':
-	    	return ComputerPlayer(symbol)
-	    else:
-	    	print("Invalid Entry.")
+        try:
+            player_type = input("Is player %s human or AI?\nEnter \'H\' for human, \'A\' for AI: " % symbol)
+        except:
+            sys.exit(0)
+        if player_type == 'H':
+            return HumanPlayer(symbol)
+        elif player_type == 'A':
+            return ComputerPlayer(symbol)
+        else:
+            print("Invalid Entry.")
 
 def pick_player(symbols):
     """Allow user to choose one of the symbols."""
     while True:
-	    print("Who should go first?\n")
-	    r_string = "Enter " +', '.join(["'%s' for player %s" % symbol for symbol in symbols]) + ":  "
-	    try:
-		    first_turn = input(r_string)
-		except:
-			sys.exit(0)
-	    if first_turn in symbols:
-		    break
-		else:
-			print("Invalid Entry.\n")
-	return first_turn
+        print("Who should go first?\n")
+        r_string = '\n'.join(["'%s' for player %s" % (symbol, symbol) for symbol in symbols]) + "\nEnter:  "
+        try:
+            first_turn = input(r_string)
+        except:
+            sys.exit(0)
+        if first_turn in symbols:
+            break
+        else:
+            print("Invalid Entry.\n")
+    return first_turn
+
+def set_order_of_play(symbols, no_turns=0):
+    while True:
+        if no_turns ==0:
+            first_turn = pick_player(symbols)
+            order_list = symbols[symbols.index(first_turn):] + symbols[:symbols.index(first_turn)]
+        no_turns +=1
+        current_turn = order_list.pop(0)
+        order_list.append(current_turn)
+        yield current_turn
 
 
-def play_game(game_board, players):
+def play_game(game_board, players, order_of_play):
     """Contains the main game loop."""
     while True:
-    	pass
+        game_board.player_turn = next(order_of_play)
+        print(game_board)
+        move = players[game_board.player_turn].make_move(game_board)
+        game_board.turn(move)
+        if game_board.done != 0:
+            break
     print(game_board)
     game_board.print_winner()
 
@@ -50,12 +65,12 @@ def main():
 
     """
     SYMBOLS = ['X','O']
-    game_board = GameBoard(3) # Creates a 3X3 game board
+    game_board = GameBoard(3)     # Creates a 3X3 game board
     players = {symbol:'' for symbol in SYMBOLS}
     for symbol in SYMBOLS:
-	    players[symbol] = create_player(symbol)
-    game_board.player_turn = pick_player(SYMBOLS)
-    play_game(game_board, players)
+        players[symbol] = create_player(symbol)
+    order_of_play = set_order_of_play(SYMBOLS)
+    play_game(game_board, players, order_of_play)
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -7,13 +7,13 @@ class GameBoard(object): # all classes should be new-style
     """Handles the current and future states of a tic-tac-toe board.
 
     Encompasses GameState, some of get_user_move, generate_game_tree 
-    and print_board.
+    and __print_board.
 
     """
     def __init__(self, size):
         self.clear = "\n" * 100
-        self.player_turn = None
-        assert size >2 and < 9
+        self.player_turn = ' '
+        assert size > 2 and size < 9
         self.size = size
         self.moves = [' '] * size * size
         self.done = 0
@@ -22,12 +22,12 @@ class GameBoard(object): # all classes should be new-style
         # Clear the board
         result = self.clear
         # Print Player's Turn
-        result += "Your turn (Player %s)" % (self.player_turn )
+        result += "Your turn (Player %s)\n" % (self.player_turn )
         # Print Board
         result += self.__print_board(self.moves)
         # Print Legend
         result += "\n Legend:\n"
-        result +=  self.__print_board([ord(i) for i in range(65, 65 +(self.size *self.size))])
+        result +=  self.__print_board([chr(i) for i in range(65, 65 +(self.size *self.size))])
         return result
 
     def __print_board(self, positions):
@@ -35,7 +35,7 @@ class GameBoard(object): # all classes should be new-style
         for _ in range(self.size):
             move_string += '|%s'*self.size
             move_string += '\n'
-        return (move_string % positions)
+        return (move_string % tuple(positions))
 
     def get_user_move(self):
         while True:
@@ -49,27 +49,57 @@ class GameBoard(object): # all classes should be new-style
             if move < 0 or move > (self.size*self.size):
                 print("Input is not a valid move. Please enter a move from the Legend.")
             # Check that the move has not already been played
-            elif self.moves[self.move] != ' ':
+            elif self.moves[move] != ' ':
                 print("Move is already taken. Pick another move")
             else:
                 break
         return move
 
-    def play_move(self, move, player):
-        self.moves[move] = player.symbol
+    def turn(self, move):
+        self.moves[move] = self.player_turn
+        if self.check_win():
+            self.winners =[self.player_turn]
+        elif self.check_tie():
+            self.winners = [' ']
+            self.done = -1
 
-    def generate_game_tree():
+    def generate_game_tree(self, computer_player):
         pass
+
+    def check_win(self):
+        is_win =False
+        locations = range(self.size * self.size)
+        horizontals = [self.moves[i*self.size:(i+1)*self.size] for i in range(self.size)]
+        verticals = [[self.moves[i+k*self.size] for k in range(self.size)] for i in range(self.size)]
+        diagonals = [[self.moves[i*self.size+i] for i in range(self.size)],[self.moves[(i+1)*self.size -(i+1)] for i in range(self.size)]]
+        for line in horizontals+verticals+diagonals:
+            is_win = self.check_win_line(line)
+            if is_win:
+                self.done = 1
+                break
+        return is_win
+
+    def check_win_line(self, line):
+        if any([l==' 'for l in line]):
+            return False
+        elif all([line[0]==line[i] for i in range(self.size)]):
+            return True
+        else:
+            return False
+
+    def check_tie(self):
+        if any([m==' ' for m in self.moves]):
+            return False
+        return True
+
 
     def print_winner(self):
         if self.done ==0:
             print("But the game isn't over yet!")
-        elif len(self.winners) == 1:
-            print("The winner is player %s!" % (self.winners[0].symbol))
-        else:
+        elif self.done == 1:
+            print("The winner is player %s!" % tuple(self.winners))
+        elif self.done == -1:
             print("It's a tie!")
-            for winner in self.winners:
-                print("Congratulations player %s!\n" % winner.symbol)
 
 
 
@@ -80,7 +110,7 @@ class Player(object):
     (which will just 'raise NotImplementedError' here).
 
     """
-    def __init(self, symbol):
+    def __init__(self, symbol):
         self.symbol = symbol
     def make_move(self):
         raise NotImplementedError
@@ -92,7 +122,9 @@ class HumanPlayer(Player):
     Encompasses the rest of get_user_move.
 
     """
-    pass
+    def make_move(self, game_board):
+        return game_board.get_user_move()
+        
 
 
 class ComputerPlayer(Player):
@@ -101,4 +133,7 @@ class ComputerPlayer(Player):
     Encompasses generate_best_move and minimax.
 
     """
-    pass
+    def generate_best_move(self):
+        pass
+    def minimax(self):
+        pass
